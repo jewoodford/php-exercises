24af3b3d670e4b4ba06a5dc0c4f216695741cb46
Justin Woodford <jewoodford@Justins-Necronomicon.local>
2015-11-06 10:43:15 -0600

    finished arguments exercise

--- arithmetic.php
+++ arithmetic.php
@@ -7,55 +7,68 @@ function validate($a, $b) {
 	}
 }
 
+function errorMessage($a, $b) {
+	if (validate($a, $b)) {
+		return true;
+	} else {
+		return 'ERROR: Both arguments must be numeric ' . $a . ' ' . $b;
+	}
+}
+
 function add($a, $b)
 {	
-	if (validate($a, $b)) {
+	$isValid = errorMessage($a, $b);
+	if ($isValid === true) {
 		return $a + $b;
 	} else {
-		return 'ERROR: Bothe arguments must be numeric ' . $a . ' ' . $b . PHP_EOL;
+		return $isValid;
 	}
 }
 
 function subtract($a, $b)
 {
+	$isValid = errorMessage($a, $b);
 	if (validate($a, $b)) {
 		return $a - $b;
 	} else {
-		return 'ERROR: Bothe arguments must be numeric ' . $a . ' ' . $b . PHP_EOL;
+		return $isValid;
 	}
 }
 
 function multiply($a, $b)
 {
+	$isValid = errorMessage($a, $b);
 	if (validate($a, $b)) {
 		return $a * $b;
 	} else {
-		return 'ERROR: Bothe arguments must be numeric ' . $a . ' ' . $b . PHP_EOL;
+		return $isValid;
 	}
 }
 
 function divide($a, $b)
 {
 	if ($b == 0) {
-		return "ERROR: You are trying to destroy us all! " . $a . ' ' . $b . PHP_EOL;
+		return "ERROR: You are trying to destroy us all! " . $a . ' ' . $b;
 	}
+	$isValid = errorMessage($a, $b);
 	if (validate($a, $b)) {
 		return $a / $b;
 	} else {
-		return 'ERROR: Bothe arguments must be numeric ' . $a . ' ' . $b . PHP_EOL;
+		return $isValid;
 	}
 }
 
 function modulus($a, $b)
 {
+	$isValid = errorMessage($a, $b);
 	if (validate($a, $b)) {
 		return $a % $b;
 	} else {
-		return 'ERROR: Bothe arguments must be numeric ' . $a . ' ' . $b . PHP_EOL;
+		return $isValid;
 	}
 }
 
-echo add(9, 3) . PHP_EOL;
+echo add(9, "notNumeric") . PHP_EOL;
 echo subtract(9, 3) . PHP_EOL;
 echo multiply(9, 3) . PHP_EOL;
 echo divide(9, 0) . PHP_EOL;
