185346817dba99f177ac8c5c759d3edada5843cc
Justin Woodford <jewoodford@Justins-Necronomicon.local>
2015-11-06 10:08:22 -0600

    arithmetic exercise

--- arithmetic.php
+++ arithmetic.php
@@ -2,35 +2,35 @@
 
 function add($a, $b)
 {
-    return $a + $b . PHP_EOL;
+    return $a + $b;
 }
 
 function subtract($a, $b)
 {
-	return $a - $b . PHP_EOL;
+	return $a - $b;
     // Add code here
 }
 
 function multiply($a, $b)
 {
-	return $a * $b . PHP_EOL;
+	return $a * $b;
     // Add code here
 }
 
 function divide($a, $b)
 {
-	return $a / $b . PHP_EOL;
+	return $a / $b;
     // Add code here
 }
 
 function modulus($a, $b)
 {
-	return $a % $b . PHP_EOL;
+	return $a % $b;
 }
 
-echo add(9, 3);
-echo subtract(9, 3);
-echo multiply(9, 3);
-echo divide(9, 3);
-echo modulus(9, 3);
+echo add(9, 3) . PHP_EOL;
+echo subtract(9, 3) . PHP_EOL;
+echo multiply(9, 3) . PHP_EOL;
+echo divide(9, 3) . PHP_EOL;
+echo modulus(9, 3) . PHP_EOL;
 // Add code to test your functions here
