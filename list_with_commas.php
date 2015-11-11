e2c412270f9f413fb571d01dc035877d2764fa57
Justin Woodford <jewoodford@Justins-Necronomicon.local>
2015-11-11 09:55:46 -0600

    finished exercise

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,23 +1,25 @@
 <?php
 
- // Converts array into list n1, n2, ..., and n3
- function humanizedList($array) {
- 	sort($array);
-   $lastItem = array_pop($array);
-   $humanLastItem = 'and ' . $lastItem;
-   array_push($array, $humanLastItem);
-   $physicistList = implode(', ', $array);
-   return $physicistList;
- }
+// Converts array into list n1, n2, ..., and n3
+function humanizedList($array, $sort = false) {
+	if ($sort) {
+		sort($array);
+	}
+	$lastItem = array_pop($array);
+	$humanLastItem = 'and ' . $lastItem;
+	array_push($array, $humanLastItem);
+	$physicistList = implode(', ', $array);
+	return $physicistList;
+}
 
- $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
- $physicistsArray = explode(', ', $physicistsString);
+$physicistsArray = explode(', ', $physicistsString);
 
 
- $famousFakePhysicists = humanizedList($physicistsArray);
+$famousFakePhysicists = humanizedList($physicistsArray, true);
 
- // Output sentence
- echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
+// Output sentence
+echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
 
- ?>
+?>
