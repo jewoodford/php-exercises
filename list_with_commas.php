a2b38f2cecd541c3fafd3ad345796a1fed76bc0d
Justin Woodford <jewoodford@Justins-Necronomicon.local>
2015-11-11 09:46:28 -0600

    finished? Step 2 of Explode and Implode exercise

---
+++ list_with_commas.php
@@ -0,0 +1,22 @@
+<?php
+
+ // Converts array into list n1, n2, ..., and n3
+ function humanizedList($array) {
+   $lastItem = array_pop($array);
+   $humanLastItem = 'and ' . $lastItem;
+   array_push($array, $humanLastItem);
+   $physicistList = implode(', ', $array);
+   return $physicistList;
+ }
+
+ $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+ $physicistsArray = explode(', ', $physicistsString);
+
+
+ $famousFakePhysicists = humanizedList($physicistsArray);
+
+ // Output sentence
+ echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
+
+ ?>
